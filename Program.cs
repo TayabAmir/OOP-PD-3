0e5a1d122c8320811a2bb92bd9e0d3a151b92e75
TayabAmir <144408356+TayabAmir@users.noreply.github.com>
2024-02-04 05:18:31 -0800

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,19 +4,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ScientificCal
+namespace Calculator
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             int opt;
-            bool flag = false, flag1 = false;
-            sCal calculator = new sCal();
+            bool flag = false;
+            Cal calculator = new Cal();
 
             do
             {
-                Console.Clear();
+
                 Console.WriteLine("1.Create the a Single Object of Calculator");
                 Console.WriteLine("2.Change Values of Attributes");
                 Console.WriteLine("3.Add");
@@ -24,22 +24,17 @@ namespace ScientificCal
                 Console.WriteLine("5.Multiply");
                 Console.WriteLine("6.Divide");
                 Console.WriteLine("7.Modulo");
-                Console.WriteLine("8.Enter the value for below functions");
-                Console.WriteLine("9.Square Root");
-                Console.WriteLine("10.exponential");
-                Console.WriteLine("11.logarithm");
-                Console.WriteLine("12.Trignometric Function");
                 Console.WriteLine("0.Exit");
                 Console.Write("Choose the option...");
                 opt = int.Parse(Console.ReadLine());
 
-                if (opt == 1)
+                if(opt==1)
                 {
                     calculator.num1 = 10;
                     calculator.num2 = 10;
                     flag = true;
                 }
-                else if (opt == 2 && flag)
+                else if(opt==2 && flag==true)
                 {
                     Console.Write("Enter num1: ");
                     calculator.num1 = int.Parse(Console.ReadLine());
@@ -47,19 +42,19 @@ namespace ScientificCal
                     calculator.num2 = int.Parse(Console.ReadLine());
                 }
 
-                else if (opt == 3 && flag)
+                else if (opt == 3 && flag == true)
                 {
                     Console.WriteLine(calculator.addition());
                 }
-                else if (opt == 4 && flag)
+                else if (opt == 4 && flag == true)
                 {
                     Console.WriteLine(calculator.subtraction());
                 }
-                else if (opt == 5 && flag)
+                else if (opt == 5 && flag == true)
                 {
                     Console.WriteLine(calculator.multiplication());
                 }
-                else if (opt == 6 && flag)
+                else if (opt == 6 && flag == true)
                 {
                     if (calculator.division() != -1)
                     {
@@ -70,7 +65,7 @@ namespace ScientificCal
                         Console.WriteLine("Second number must not equal to zero");
                     }
                 }
-                else if (opt == 7 && flag)
+                else if (opt == 7 && flag == true)
                 {
                     if (calculator.modulus() != -1)
                     {
@@ -81,63 +76,14 @@ namespace ScientificCal
                         Console.WriteLine("Second number must not equal to zero");
                     }
                 }
-                else if (opt == 8 && flag)
+                else if(!flag)
                 {
-                    Console.Write("Enter the number: ");
-                    calculator.num = int.Parse(Console.ReadLine());
-                    flag1 = true;
+                    Console.WriteLine("Select option 1 first to create object");
                 }
-                else if (opt == 9)
-                {
-                    if (flag1)
-                       Console.WriteLine(calculator.squareRoot());
-                    else
-                        Console.Write("First go to option 8");
-                    Console.ReadKey();
-
-                    continue;
-                }
-                else if (opt == 10)
-                {
-                    if (flag1)
-                        Console.WriteLine(calculator.exponential());
-                    else
-                        Console.Write("First go to option 8");
-                    Console.ReadKey();
-                    continue;
-                }
-                else if (opt == 11)
-                {
-                    if (flag1)
-                        Console.WriteLine(calculator.logarithm());
-                    else
-                        Console.Write("First go to option 8");
-                    Console.ReadKey();
-
-                    continue;
-
-                }
-                else if (opt == 12)
-                {
-                    if (flag1)
-                    {
-                        Console.WriteLine("SIN of number is: " + calculator.sine());
-                        Console.WriteLine("COS of number is: " + calculator.cosine());
-                        Console.WriteLine("TAN of number is: " + calculator.tangent());
-                    }
-                    else
-                        Console.Write("First go to option 8");
-                    Console.ReadKey();
-
-                    continue;
-
-                }
-                if (!flag)
-                    Console.WriteLine("First go to option 1 to create object");
                 Console.ReadKey();
-            }while (opt != 0);
+            }
+            while (opt!=0);
             Console.WriteLine("Thank You");
-
         }
     }
 }
